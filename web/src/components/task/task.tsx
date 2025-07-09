c375926d00445effe41268631bd7a65c1123ecec
João Trajano <joao.trajanosouza@gmail.com>
2025-07-09 12:24:04 -0300

    feat: add the checkbox in the task component, and change the theme to blue

--- web/src/components/task/task.tsx
+++ web/src/components/task/task.tsx
@@ -1,14 +1,27 @@
+import { Button } from "../ui/button";
+import { Trash } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
+
 type Props = {
   id: string;
   title: string;
-  createdAt: Date;
+  createdAt: string;
 };
 
 export const Task = ({ id, title, createdAt }: Props) => {
   return (
-    <div className="justify-between">
-      <div></div>
-      <div></div>
+    <div className="flex justify-between items-center p-4 border rounded-xl shadow-sm">
+      <div className="flex items-center gap-2">
+        <Checkbox />
+        <span className="text-base text-gray-800">{title}</span>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <span className="text-sm text-gray-500">{createdAt}</span>
+        <Button variant="ghost" size="icon">
+          <Trash className="w-5 h-5" />
+        </Button>
+      </div>
     </div>
   );
 };
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,5 @@
+import { Task } from "@/components/task";
+
 export default function Home() {
-  return (
-    <main className="flex justify-center items-center h-screen">
-      <h2 className="text-2xl font-bold">Tarefas</h2>
-    </main>
-  );
+  return <Task title="First task" id="123" createdAt="123" />;
 }
