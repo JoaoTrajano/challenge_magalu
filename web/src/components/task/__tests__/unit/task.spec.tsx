73aedf6db88089ecee23126579e2331b5532311a
João Trajano <joao.trajanosouza@gmail.com>
2025-07-10 16:09:39 -0300

    test: create unit test for the component tasks

---
+++ web/src/components/task/__tests__/unit/task.spec.tsx
@@ -0,0 +1,91 @@
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { Tasks, TaskStatus } from "@/api/tasks/@types";
+import { beforeEach, expect, Mock, test, vi } from "vitest";
+import { Task } from "../../task";
+
+vi.mock("@/api/tasks", () => ({
+  useDeleteTask: vi.fn(),
+  useUpdateTaskStatus: vi.fn(),
+}));
+
+vi.mock("@tanstack/react-query", () => ({
+  useQueryClient: vi.fn(),
+}));
+
+vi.mock("sonner", () => ({
+  toast: {
+    error: vi.fn(),
+  },
+}));
+
+import * as apiTasks from "@/api/tasks";
+import * as reactQuery from "@tanstack/react-query";
+
+const mockDelete = vi.fn();
+const mockUpdate = vi.fn();
+const mockInvalidateQueries = vi.fn();
+
+const mockUseDeleteTask = apiTasks.useDeleteTask as unknown as Mock;
+const mockUseUpdateTaskStatus = apiTasks.useUpdateTaskStatus as unknown as Mock;
+const mockUseQueryClient = reactQuery.useQueryClient as unknown as Mock;
+
+beforeEach(() => {
+  mockUseDeleteTask.mockReturnValue({
+    mutateAsync: mockDelete,
+    isPending: false,
+  });
+
+  mockUseUpdateTaskStatus.mockReturnValue({
+    mutateAsync: mockUpdate,
+    isPending: false,
+  });
+
+  mockUseQueryClient.mockReturnValue({
+    invalidateQueries: mockInvalidateQueries,
+    getQueryData: vi.fn(() => ({
+      value: [],
+    })),
+    setQueryData: vi.fn(),
+  });
+});
+
+const taskMock: Tasks = {
+  id: "123",
+  title: "Some task title",
+  status: TaskStatus.PENDING,
+  createdAt: new Date("2023-10-01T12:00:00Z"),
+  updatedAt: new Date("2023-10-01T12:00:00Z"),
+};
+
+test("renderiza título e data da tarefa", () => {
+  render(<Task data={taskMock} />);
+
+  expect(screen.getByText("Some task title")).toBeInTheDocument();
+  expect(screen.getByText(/Criado em:/)).toBeInTheDocument();
+});
+
+test("deleta a tarefa ao clicar no ícone de lixeira", async () => {
+  render(<Task data={taskMock} />);
+  const deleteButton = screen.getByRole("button");
+
+  fireEvent.click(deleteButton);
+
+  await waitFor(() => {
+    expect(mockDelete).toHaveBeenCalledWith({ id: taskMock.id });
+  });
+});
+
+test("marca a tarefa como COMPLETED ao clicar no checkbox", async () => {
+  render(<Task data={taskMock} />);
+
+  const checkbox = screen.getByRole("checkbox");
+
+  fireEvent.click(checkbox);
+
+  await waitFor(() => {
+    expect(mockUpdate).toHaveBeenCalledWith({
+      id: taskMock.id,
+      newStatus: TaskStatus.COMPLETED,
+    });
+  });
+});
