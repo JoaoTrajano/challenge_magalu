690f22e4d26c7dcdfae6bbae9235b302f6d90cc6
João Trajano <joao.trajanosouza@gmail.com>
2025-07-10 10:21:29 -0300

    refactor: use isRefetching

--- web/src/components/task-list/task-list.tsx
+++ web/src/components/task-list/task-list.tsx
@@ -4,20 +4,29 @@ import { useMemo } from "react";
 import SkeletonTaskList from "./components/skeleton-task-list";
 
 export const TaskList = () => {
-  const { data: responseFetchTasks, isLoading } = useFetchTasks({});
+  const {
+    data: responseFetchTasks,
+    isLoading,
+    isRefetching,
+  } = useFetchTasks({});
 
   const tasks = useMemo(
     () => (responseFetchTasks ? responseFetchTasks.value : []),
     [responseFetchTasks]
   );
 
+  if (tasks.length === 0) {
+    return (
+      <div className="text-gray-500 text-center py-4">
+        Nenhuma tarefa adicionada.
+      </div>
+    );
+  }
+
   return (
     <div className="space-y-2">
-      {isLoading ? <SkeletonTaskList /> : null}
-      {tasks.length === 0 ? (
-        <div className="text-gray-500 text-center py-4">
-          Nenhuma tarefa adicionada.
-        </div>
+      {isLoading || isRefetching ? (
+        <SkeletonTaskList />
       ) : (
         tasks.map((task) => (
           <Task
